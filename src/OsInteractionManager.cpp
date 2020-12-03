bde6ba068b989da9ac7c1a6bae0b444d1de6a3e2
Andrey Sokolov <sokolov19830711@gmail.com>
2020-12-03 21:36:53 +0300

    Добпвлена функция автозагрузки и блокировки системы по Линукс

--- src/OsInteractionManager.cpp
+++ src/OsInteractionManager.cpp
@@ -1,7 +1,12 @@
 #include "OsInteractionManager.h"
 
-#include <QDir>
 #include <QCoreApplication>
+
+#include <QDir>
+#include <QFile>
+#include <QTextStream>
+#include <QStandardPaths>
+#include <QProcess>
 
 #ifdef Q_OS_WIN32
 #include <windows.h>
@@ -36,6 +41,60 @@ void OsInteractionManager::setStartOnBoot(bool state) const
 		settings.remove("ONREMOTE+");
 	}
 #endif // Q_OS_WIN32
+#ifdef Q_OS_LINUX
+
+    // Путь к папке автозапуска
+    QString autostartPath = QStandardPaths::standardLocations(QStandardPaths::ConfigLocation).at(0) + QLatin1String("/autostart");
+    /* Проверяем, существует ли директория, в которой будет храниться файл автозапуска.
+         * А то мало ли... пользователь удалил...
+         * */
+    QDir autorunDir(autostartPath);
+    if(!autorunDir.exists()){
+        // Если не существует, то создаём
+        autorunDir.mkpath(autostartPath);
+    }
+    QFile autorunFile(autostartPath + QLatin1String("/ONREMOTE+.sh.desktop"));
+
+    if(state)
+    {
+        if(!autorunFile.exists())
+        {
+
+            /* Далее открываем файл и записываем в него необходимые данные
+                     * с указанием пути к исполняемому файлу, с помощью QCoreApplication::applicationFilePath()
+                     * */
+            if(autorunFile.open(QFile::WriteOnly))
+            {
+                QDir scriptDir(QCoreApplication::applicationFilePath());
+                scriptDir.cdUp();
+                QString filePathToScript = scriptDir.absolutePath() + "/ONREMOTE+.sh";
+                QString autorunContent("[Desktop Entry]\n"
+                                       "Type=Application\n"
+                                       "Exec=" + filePathToScript + "\n"
+                                                                    "Hidden=false\n"
+                                                                    "NoDisplay=false\n"
+                                                                    "X-GNOME-Autostart-enabled=true\n"
+                                                                    "Name[en_GB]=ONREMOTE+\n"
+                                                                    "Name=ONREMOTE+\n"
+                                                                    "Comment[en_GB]=ONREMOTE+\n"
+                                                                    "Comment=ONREMOTE+\n");
+                QTextStream outStream(&autorunFile);
+                outStream << autorunContent;
+                // Устанавливаем права доступа, в том числе и на исполнение файла, иначе автозапуск не сработает
+                autorunFile.setPermissions(QFileDevice::ExeUser|QFileDevice::ExeOwner|QFileDevice::ExeOther|QFileDevice::ExeGroup|
+                                           QFileDevice::WriteUser|QFileDevice::ReadUser);
+                autorunFile.close();
+            }
+        }
+    }
+
+    else
+    {
+        // Удаляем файл автозапуска
+        if(autorunFile.exists()) autorunFile.remove();
+    }
+
+#endif
 }
 
 void OsInteractionManager::setLockOS(bool state) const
@@ -49,6 +108,12 @@ void OsInteractionManager::lockOS() const
 	{
 #ifdef Q_OS_WIN32
 		LockWorkStation();
+#endif
+
+#ifdef Q_OS_LINUX
+        QProcess::execute("gnome-session-quit", {"--force", "--no-prompt"});
+        QProcess::execute("mate-session-save", {"--force-logout"});
+        QProcess::execute("cinnamon-session-quit", {"--logout", "--force"});
 #endif
 	}
 }
