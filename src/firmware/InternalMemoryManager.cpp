ac10133d52dbb84418428b42e81a7a80d95ecf66
Andrey Sokolov <sokolov19830711@gmail.com>
2020-12-12 20:48:55 +0300

    Исправлен сброс настроек при загрузке

--- src/firmware/InternalMemoryManager.cpp
+++ src/firmware/InternalMemoryManager.cpp
@@ -14,7 +14,7 @@ InternalMemoryManager::InternalMemoryManager()
 
 void InternalMemoryManager::initConfig()
 {
-    if (eeprom_read_byte(0) == 123 && !digitalRead(PC_POWER_BUTTON))
+    if (eeprom_read_byte(0) == 123 && digitalRead(PC_POWER_BUTTON))
     {
         eeprom_read_block((void*)(&DataManager::config()), 1, sizeof(McuInData));
     }
