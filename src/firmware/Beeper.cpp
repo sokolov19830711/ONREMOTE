68a0ab6456c0f10cba35a42ca9cec223b660f374
Andrey Sokolov <sokolov19830711@gmail.com>
2020-12-26 21:12:19 +0300

    Изменил наследование Beeper с private на public

--- src/firmware/Beeper.cpp
+++ src/firmware/Beeper.cpp
@@ -1,38 +1,5 @@
 #include "Beeper.h"
 
-#include <Arduino.h>
-#include "Pinout.h"
-
-//Beeper::Beeper() : Pin(BEEPER, LOW, Pin::SignalType::digital, OUTPUT)
-//{
-//
-//}
-//
-//Beeper& Beeper::getInstance()
-//{
-//	static Beeper instance;
-//	return instance;
-//}
-//
-void Beeper::init(int pin, int idleValue, SignalType signalType, int pinMode)
-{
-	PinSingletonInterface::init(pin, idleValue, signalType, pinMode);
-	//getInstance().setupSequence(new Signal[2]{
-	//	{HIGH, 150},
-	//	{LOW, 150}
-	//	/*{HIGH, 150},
-	//	{LOW, 150},
-	//	{HIGH, 150},
-	//	{LOW, 150},
-	//	{HIGH, 500},
-	//	{LOW, 150},
-	//	{HIGH, 500},
-	//	{LOW, 150},
-	//	{HIGH, 500},
-	//	{LOW, 150}*/
-	//	}, 2);
-}
-
 void Beeper::beep()
 {
 	getInstance().setupSequence(new Signal[2]{ {HIGH, 150}, {LOW, 150} }, 2);
--- src/firmware/Beeper.h
+++ src/firmware/Beeper.h
@@ -2,16 +2,14 @@
 
 #include "PinSingletonInterface.h"
 
-class Beeper : private PinSingletonInterface
+class Beeper : public PinSingletonInterface
 {
 public:
 
-	static void init(int pin, int idleValue = LOW, SignalType signalType = digital, int pinMode = INPUT);
 	static void beep();
 
 private:
 
 	Beeper();
-//	static Beeper& getInstance();
 };
 
